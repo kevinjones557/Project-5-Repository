3ccc5752a0bf509b83e2c9cb7b120bf1f1f3b64e
johndavbrooks <112205825+johndavbrooks@users.noreply.github.com>
2022-11-27 22:31:57 -0500

    Update Server.java
    append, delete, and edit receiving functions to execute messaging

--- Server.java
+++ Server.java
@@ -7,4 +7,77 @@
  * @version 11/20
  */
 public class Server {
+  
+  
+  
+  public static void appendReceive(BufferedReader reader) {
+        try {
+            String personData = reader.readLine();
+            String sender = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String recipient = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String storeName = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String buyer = personData.substring(0, personData.indexOf(","));
+            boolean isBuyer = false;
+            if (buyer.equals("true"))
+                isBuyer = true;
+
+            String message = reader.readLine();
+
+            Message.appendMessage(sender, recipient, storeName, isBuyer, message);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "The data could not be handled.", "Messaging System",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+  
+  public static void deleteReceive(BufferedReader reader) {
+        try {
+            String personData = reader.readLine();
+            String sender = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String recipient = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String storeName = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String buyer = personData.substring(0, personData.indexOf(","));
+            boolean isBuyer = false;
+            if (buyer.equals("true"))
+                isBuyer = true;
+
+            String message = reader.readLine();
+
+            Message.deleteMessage(sender, recipient, storeName, isBuyer, message);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "The data could not be handled.", "Messaging System",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+  
+  public static void editReceive(BufferedReader reader) {
+        try {
+            String personData = reader.readLine();
+            String sender = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String recipient = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String storeName = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String buyer = personData.substring(0, personData.indexOf(","));
+            boolean isBuyer = false;
+            if (buyer.equals("true"))
+                isBuyer = true;
+
+            String messageToEdit = reader.readLine();
+            String edit = reader.readLine();
+
+            Message.editMessage(sender, recipient, storeName, isBuyer, messageToEdit, edit);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "The data could not be handled.", "Messaging System",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+  
 }
