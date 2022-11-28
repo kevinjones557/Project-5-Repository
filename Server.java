324d5e1e2a1e542795f35e17958837c34f1e1e13
Kevin Jones <twinbaseball.kpj@gmail.com>
2022-11-27 23:06:42 -0500

    Lots of gui changes and some client server work

--- Server.java
+++ Server.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,4 +13,155 @@ import java.util.LinkedHashMap;
  * @version 11/20
  */
 public class Server {
+    public static void main(String[] args) throws IOException {
+        LinkedHashMap<String, String> storeNameMap = FileManager.mapStoresToSellers();
+
+        ServerSocket serverSocket = new ServerSocket(2000); // port 2000
+        Socket socket = serverSocket.accept();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+        while (true) {
+            String line = reader.readLine();
+            String instruction = line.substring(0, line.indexOf(';'));
+            String contents = line.substring(line.indexOf(";") + 1);
+            if (instruction.equals("GetSeller")) {
+                // line format: GetSeller;<StoreName> without surrounding carrots
+                writer.println(storeNameMap.get(contents));
+                writer.flush();
+            } else if (instruction.equals("CheckIfMessageExists")) {
+                // line format: CheckIfMessageExists;<recipient>;<isRecipientStore>;<isSeller>;<username>;<isUserStore>
+                // without surrounding carrots
+                String recipient = contents.substring(0, contents.indexOf(";"));
+                contents = contents.substring(contents.indexOf(";") + 1);
+                boolean isRecipientStore = Boolean.parseBoolean(contents.substring(0, contents.indexOf(";")));
+                contents = contents.substring(contents.indexOf(";") + 1);
+                boolean isSeller = Boolean.parseBoolean(contents.substring(0, contents.indexOf(";")));
+                contents = contents.substring(contents.indexOf(";") + 1);
+                String username = (contents.substring(0, contents.indexOf(";")));
+                contents = contents.substring(contents.indexOf(";") + 1);
+                boolean isUserStore = Boolean.parseBoolean(contents);
+                checkIfMessageExists(recipient, isRecipientStore, isSeller, username, isUserStore, storeNameMap);
+            }
+        }
+    }
+
+    public static void checkIfMessageExists(String recipient, boolean isRecipientStore, boolean isSeller,
+                                            String username, boolean isUserStore, LinkedHashMap storeNameMap) {
+        // check if <username><recipient>.txt exits in directory or not
+        if (!isRecipientStore) {
+            String path1 = "";
+            String path2 = "";
+            if (isSeller) {
+                path1 = "data/sellers/" + username + "/";
+                path2 = "data/buyers/" + recipient + "/";
+                if (isUserStore) {
+                    path1 = "data/sellers/" + storeNameMap.get(username) + "/" + username + "/";
+                }
+            } else {
+                path1 = "data/buyers/" + username + "/";
+                path2 = "data/sellers/" + recipient + "/";
+            }
+            try {
+                System.out.println(path1);
+                System.out.println(path2);
+                File fUser = new File(path1 + username + recipient + ".txt");
+                boolean didCreate = fUser.createNewFile();
+                if (didCreate) {
+                    File fRecipient = new File(path2 + recipient + username + ".txt");
+                    fRecipient.createNewFile();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            FileManager.generateMetricsAboutUser(username,
+                    "data/sellers/" + storeNameMap.get(recipient)
+                            + "/" + recipient + "/");
+            try {
+                File fUser = new File(FileManager.getDirectoryFromUsername(username)
+                        + "/" + username + recipient + ".txt");
+                boolean didCreate = fUser.createNewFile();
+                if (didCreate) {
+                    String sellerName = (String) storeNameMap.get(recipient);
+                    File fRecipient = new File("data/sellers/" + sellerName
+                            + "/" + recipient + "/" + recipient + username + ".txt");
+                    fRecipient.createNewFile();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void appendReceive(BufferedReader reader) {
+        try {
+            String personData = reader.readLine();
+            String sender = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String recipient = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String storeName = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String buyer = personData.substring(0, personData.indexOf(","));
+            boolean isBuyer = false;
+            if (buyer.equals("true"))
+                isBuyer = true;
+
+            String message = reader.readLine();
+
+            Message.appendMessage(sender, recipient, storeName, isBuyer, message);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "The data could not be handled.", "Messaging System",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public static void deleteReceive(BufferedReader reader) {
+        try {
+            String personData = reader.readLine();
+            String sender = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String recipient = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String storeName = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String buyer = personData.substring(0, personData.indexOf(","));
+            boolean isBuyer = false;
+            if (buyer.equals("true"))
+                isBuyer = true;
+
+            String message = reader.readLine();
+
+            Message.deleteMessage(sender, recipient, storeName, isBuyer, message);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "The data could not be handled.", "Messaging System",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public static void editReceive(BufferedReader reader) {
+        try {
+            String personData = reader.readLine();
+            String sender = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String recipient = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String storeName = personData.substring(0, personData.indexOf(","));
+            personData = personData.substring(personData.indexOf(",") + 1);
+            String buyer = personData.substring(0, personData.indexOf(","));
+            boolean isBuyer = false;
+            if (buyer.equals("true"))
+                isBuyer = true;
+
+            String messageToEdit = reader.readLine();
+            String edit = reader.readLine();
+
+            Message.editMessage(sender, recipient, storeName, isBuyer, messageToEdit, edit);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "The data could not be handled.", "Messaging System",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
 }
